decf6fa7da12a0613f99edd5a77f2572363d4c81
Marcos RZ <marcosviniciosrezende5@gmail.com>
2026-05-11 10:45:46 -0300

    teste01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello, world!</h1>
+    <><h1>Hello, world!</h1><div><Button> teste</Button> </div></>
   );
 }
